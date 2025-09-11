73e0b6de925a6dbce7508f60c2c41a85346c3b7a
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-09-11 13:51:54 +0200

    [Added] start out-code.

--- SmartSubscription.sol
+++ SmartSubscription.sol
@@ -1,1 +1,37 @@
 // The Smart Contact for my subscription service
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract SmartSub {
+    address public owner;
+    
+    enum subStatus { Active, Paused }
+
+    struct Sub {
+        address owner;
+        uint256 fee;
+        uint256 periodLength;
+        bool paused;
+        uint256 balance;
+    }
+
+    mapping(address => Sub) public subs;
+    mapping(address => bool) public isRegistered;
+    uint256 public eventCounter;
+
+    constructor() {
+        owner = msg.sender;
+        eventCounter = 0;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function paySub(address _subscriber, uint256 _fee) public {
+        require(subs[_subscriber].owner != address(0), "Subscriber not registered");
+    }
+}
+
