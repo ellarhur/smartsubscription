b959cc8133355b5c3cc4789ad04eb7625ace6062
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-09-12 14:14:54 +0200

    [Added] starting code for first function.

--- SmartSub.sol
+++ SmartSub.sol
@@ -41,17 +41,29 @@ contract SmartSub {
 uint256 public subscriptionCounter;
 uint256 public subscriberCounter;
 
-
-    mapping(address => Subscription) public subs;
-    mapping(address => bool) public isRegistered;
-    uint256 public eventCounter;
+// Mappings
+mapping(uint256 => address) public ownerAddress; // Mapping for the owner address of subscriptions
+mapping(uint256 => mapping(address => Subscription)) public subscriptions; // Mapping for if the subscribers has an active subscription
 
     // Constructor
     constructor() {
-        owner = msg.sender;
-        eventCounter = 0;
+    owner = msg.sender;
+    subscriptionCounter = 0;
+    subscriberCounter = 0;
+    }
+
+    function addSub(string memory subTitle, uint256 subFee, uint256 periodLength) public {
+        for (uint256 i = 0; i < subscriptionCounter; i++) {
+                require(subscriptions[i].owner != msg.sender || keccak256(bytes(subscriptions[i].title)) == keccak256(bytes(subTitle)), "Owner already made this subscription"); // Make sure the owner hasn't already created this subscription.
+                require(periodLength > 0, "You have to set a period for the subscription"); // Make sure the owner sets a period
+        }
+        subscriptionCounter++;
+        subscriptions[subscriptionCounter][msg.sender] = Subscription(subscriptionCounter, _title, msg.sender, _fee, _periodLength, subStatus.Active, block.timestamp, block.timestamp + _periodLength, 0);
+        ownerAddress[subscriptionCounter] = msg.sender;
     }
 }
+
+
 
 function paySub(address _subscriber, uint256 _fee) public nonReentrant { 
 }
